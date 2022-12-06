69957792cce857f92180c0d812ed16d8d1644b6c
TECH I.S <techi.s.@TECHs-MacBook-Air-9.local>
2022-12-07 03:22:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+
+import './App.css';
+import { useState } from 'react';
+import {Task} from './Task'
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    const newTodoList = [...todoList, task];
+    setTodoList(newTodoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+  const clearTask = (id) => {
+    let key = todoList.filter((todo)=> todo.id === id)
+    if(key[0].completed){
+      setTodoList(todoList.filter((task) => task.id !== id));
+    }else{
+      setTodoList(todoList)
+      alert('complete the task')
+    }
+    
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  return (
+  <div className='App'>
+    <div className='addTask'>
+      <input placeholder='Enter Task' onChange={handleChange}/>
+      <button id='addButton' onClick={addTask}> Add Task </button>
+    </div>
+    <div className='list'>
+      {todoList.map((task) => {
+        return <Task taskName = {task.taskName} id = {task.id} completed={task.completed} deleteTask = {deleteTask} clearTask={clearTask} completeTask={completeTask}/>;
+      })}
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Task.js
@@ -0,0 +1,17 @@
+export const Task = (props) => {
+    return (
+        <div 
+        
+        id={props.completed?  "completeButton1": "completeButton"}
+       
+        
+        className="task" >
+          <h3 style={{padding:'0px 50px'}}> { props.taskName } </h3>
+          <button id="cmplt" onClick={() => props.completeTask(props.id)}> Complete </button>
+          <button id="clr" onClick={() => props.clearTask(props.id)}> Clear </button>
+          <button id="dlt" onClick={() => props.deleteTask(props.id)}> Delete Task </button>
+
+
+        </div>
+    );
+}
